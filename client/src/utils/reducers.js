4cdfee30fcb94c4c0d2bba192cee00af394c2dfc
Alex DaRe <alexdare93@gmail.com>
2021-08-23 19:57:53 -0700

    added in redux dispatch and useselector to necessary files

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,102 @@
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
+// import {
+//   UPDATE_PRODUCTS,
+//   ADD_TO_CART,
+//   UPDATE_CART_QUANTITY,
+//   REMOVE_FROM_CART,
+//   ADD_MULTIPLE_TO_CART,
+//   UPDATE_CATEGORIES,
+//   UPDATE_CURRENT_CATEGORY,
+//   CLEAR_CART,
+//   TOGGLE_CART,
+// } from './actions';
+
+// // TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
+// export const reducer = (state, action) => {
+//   switch (action.type) {
+//     // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
+//     // using the staet which is being calle dto use and the product array of the action
+//     case UPDATE_PRODUCTS:
+//       return {
+//         ...state,
+//         products: [...action.products],
+//       };
+
+//     case ADD_TO_CART:
+//       return {
+//         ...state,
+//         cartOpen: true,
+//         cart: [...state.cart, action.product],
+//       };
+
+//     case ADD_MULTIPLE_TO_CART:
+//       return {
+//         ...state,
+//         cart: [...state.cart, ...action.products],
+//       };
+//     // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
+//     // calling case of the quantity of cart to return the state onad 
+//     case UPDATE_CART_QUANTITY:
+//       return {
+//         ...state,
+//         cartOpen: true,
+//         cart: state.cart.map((product) => {
+//           if (action._id === product._id) {
+//             product.purchaseQuantity = action.purchaseQuantity;
+//           }
+//           return product;
+//         }),
+//       };
+
+//     // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
+//     // Your comment here
+//     case REMOVE_FROM_CART:
+//       let newState = state.cart.filter((product) => {
+//         return product._id !== action._id;
+//       });
+
+//       return {
+//         ...state,
+//         cartOpen: newState.length > 0,
+//         cart: newState,
+//       };
+
+//     case CLEAR_CART:
+//       return {
+//         ...state,
+//         cartOpen: false,
+//         cart: [],
+//       };
+
+//     case TOGGLE_CART:
+//       return {
+//         ...state,
+//         cartOpen: !state.cartOpen,
+//       };
+
+//     case UPDATE_CATEGORIES:
+//       return {
+//         ...state,
+//         categories: [...action.categories],
+//       };
+
+//     case UPDATE_CURRENT_CATEGORY:
+//       return {
+//         ...state,
+//         currentCategory: action.currentCategory,
+//       };
+
+//     // TODO: Add a comment describing what the default case is for
+//     // return back original state to prevent application from crashing
+//     default:
+//       return state;
+//   }
+// };
+
+// export function useProductReducer(initialState) {
+//   return useReducer(reducer, initialState);
+// }
+
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -8,91 +106,76 @@ import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
   CLEAR_CART,
-  TOGGLE_CART,
-} from './actions';
-
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+  TOGGLE_CART
+} from "./actions";
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: '',
+  cart: [],
+  cartOpen: false
+};
+export const reducers = (state = initialState, action) => {
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // using the staet which is being calle dto use and the product array of the action
     case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-
     case ADD_TO_CART:
       return {
         ...state,
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
-
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // calling case of the quantity of cart to return the state onad 
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
         cartOpen: true,
-        cart: state.cart.map((product) => {
+        cart: state.cart.map(product => {
           if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity;
+            product.purchaseQuantity = action.purchaseQuantity
           }
-          return product;
-        }),
+          return product
+        })
       };
-
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
     case REMOVE_FROM_CART:
-      let newState = state.cart.filter((product) => {
+      let newState = state.cart.filter(product => {
         return product._id !== action._id;
       });
-
       return {
         ...state,
         cartOpen: newState.length > 0,
-        cart: newState,
+        cart: newState
       };
-
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
-        cart: [],
+        cart: []
       };
-
     case TOGGLE_CART:
       return {
         ...state,
-        cartOpen: !state.cartOpen,
+        cartOpen: !state.cartOpen
       };
-
     case UPDATE_CATEGORIES:
       return {
         ...state,
         categories: [...action.categories],
       };
-
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
-        currentCategory: action.currentCategory,
-      };
-
-    // TODO: Add a comment describing what the default case is for
-    // return back original state to prevent application from crashing
+        currentCategory: action.currentCategory
+      }
     default:
       return state;
   }
 };
-
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducers;
